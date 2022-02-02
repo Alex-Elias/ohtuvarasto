167654c286f35d037db18181f966d22a1d65d058
Alex-Elias <alex.pohjanpelto@gmail.com>
2022-02-02 15:19:58 +0200

    Coverage to 100

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,47 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_initialized_with_value_less_than_zero(self):
+        self.varasto = Varasto(-10)
+
+        # the tilavuus should be zero
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+
+    def test_initialized_with_saldo_less_than_zero(self):
+        self.varasto = Varasto(10, alku_saldo=-10)
+
+        #the saldo should be zero
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+
+    def test_alku_saldo_is_greater_than_tilavuus(self):
+        self.varasto = Varasto(10, alku_saldo=20)
+
+        #the saldo should equal the tilavuus
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_ota_varastosta_with_negative_value(self):
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-10), 0)
+
+    def test_lisaa_varastoon_negative_value(self):
+        self.varasto.lisaa_varastoon(-10)
+
+        #the saldo should not have changed with a negative number
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_lisaa_varastoon_value_greater_than_tilavuus(self):
+        self.varasto.lisaa_varastoon(20)
+
+        #the saldo should be the same size as tilavuus
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_ota_varastosta_more_than_saldo(self):
+        self.varasto.lisaa_varastoon(10)
+
+        self.assertAlmostEqual(self.varasto.ota_varastosta(20), 10)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_self_str(self):
+        self.assertEquals(str(self.varasto), "saldo = 0, vielä tilaa 10")
