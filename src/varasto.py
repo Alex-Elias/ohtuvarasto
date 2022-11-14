5dbea779ac4b6c59accfd94e64d719859bdfa049
Alex-Elias <alex.pohjanpelto@gmail.com>
2022-11-14 20:11:41 +0200

    Fix __init__

--- src/varasto.py
+++ src/varasto.py
@@ -1,5 +1,6 @@
 class Varasto:
     def __init__(self, tilavuus=0.0, alku_saldo = 0):
+        self.tilavuus = tilavuus
         if tilavuus < 0.0:
             self.tilavuus = 0.0
         if alku_saldo < 0.0:
